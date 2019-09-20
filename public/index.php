8c747bed8839b12a745dfca627b8334e4fbd0925
SimonDevelop <contact@simon-micheneau.fr>
2019-09-20 15:04:38 +0200

    feat: Support for static files (css/js/img)

--- public/index.php
+++ public/index.php
@@ -1,5 +1,22 @@
 <?php
 
+if (PHP_SAPI == 'cli-server') {
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+
+    // check the file types, only serve standard files
+    if (preg_match('/\.(?:png|js|jpg|jpeg|gif|css)$/', $file)) {
+        // does the file exist? If so, return it
+        if (is_file($file))
+            return false;
+
+        // file does not exist. return a 404
+        header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
+        printf('"%s" does not exist', $_SERVER['REQUEST_URI']);
+        return false;
+    }
+}
+
 use DI\Container;
 use Slim\Factory\AppFactory;
 
